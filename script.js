5164d904ba8b4271457a74465484716d7c271f4f
Cole Cerull <Ctcerull@gmail.com>
2023-12-11 12:18:03 -0700

    close to completion

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleDropdown(dropdownId) {
+    var dropdownContent = document.getElementById(dropdownId);
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
